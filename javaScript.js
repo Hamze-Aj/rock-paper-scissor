e2037e52fd323fd577a46749c21196b5c42780a8
HamseAj <atoahmedkalif@gmail.com>
2024-08-16 17:55:48 +0300

    just prepared the index.html and the style.css for Dom , hope the rest will not be that hard

--- javaScript.js
+++ javaScript.js
@@ -1,130 +1,128 @@
-alert("hello this is rock-paper-scissor game")
+ let humanScore = 0;
+ let computerScore = 0;
 
-let humanScore = 0;
-let computerScore = 0;
-
-const getHumanChoice = () =>{
-   const responce = prompt("Enter your choice: Rock,Paper or Scissor");
-   return responce
-}
+// const getHumanChoice = () =>{
+//    const responce = prompt("Enter your choice: Rock,Paper or Scissor");
+//    return responce
+// }
 
 
-const getComputerChoice = () =>{
-    const choice = Math.floor(Math.random() * 3)
+// const getComputerChoice = () =>{
+//     const choice = Math.floor(Math.random() * 3)
 
-    if(choice === 0){
-        return 'paper'
-    }
-    else if(choice === 1){
-        return 'rock'
-    }
-    else return 'scissor'
+//     if(choice === 0){
+//         return 'paper'
+//     }
+//     else if(choice === 1){
+//         return 'rock'
+//     }
+//     else return 'scissor'
 
 
-}
+// }
 
-const playerRound = (computerChoice,humanChoice) =>{
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice()
+// const playerRound = (computerChoice,humanChoice) =>{
+//     computerChoice = getComputerChoice();
+//     humanChoice = getHumanChoice()
 
-    if (computerChoice == 'paper' && humanChoice == 'paper') {
+//     if (computerChoice == 'paper' && humanChoice == 'paper') {
 
-        humanScore++
-        computerScore++
+//         humanScore++
+//         computerScore++
 
-        console.log(computerChoice);
+//         console.log(computerChoice);
         
 
-        alert('the game is tie')
+//         alert('the game is tie')
         
-    }
-    else if (computerChoice == 'rock' && humanChoice == 'rock') {
+//     }
+//     else if (computerChoice == 'rock' && humanChoice == 'rock') {
 
-        humanScore++
-        computerScore++
-        console.log(computerChoice);
+//         humanScore++
+//         computerScore++
+//         console.log(computerChoice);
 
          
-        alert('the game is tie')        
-    }
-    else if (computerChoice == 'scissor' && humanChoice == 'scissor') {
+//         alert('the game is tie')        
+//     }
+//     else if (computerChoice == 'scissor' && humanChoice == 'scissor') {
 
-        humanScore++
-        computerScore++
-        console.log(computerChoice);
+//         humanScore++
+//         computerScore++
+//         console.log(computerChoice);
 
-        alert('the game is tie')
+//         alert('the game is tie')
         
-    }
-    else if (computerChoice == 'paper' && humanChoice == 'rock') {
-        computerScore++
-        alert('the computer won paper beats rock')
-        console.log(computerChoice);
+//     }
+//     else if (computerChoice == 'paper' && humanChoice == 'rock') {
+//         computerScore++
+//         alert('the computer won paper beats rock')
+//         console.log(computerChoice);
         
         
-    }
-    else if (computerChoice == 'rock' && humanChoice == 'paper') {
-        humanScore++
-        alert('you won paper beats rock')
-        console.log(computerChoice);
+//     }
+//     else if (computerChoice == 'rock' && humanChoice == 'paper') {
+//         humanScore++
+//         alert('you won paper beats rock')
+//         console.log(computerChoice);
 
      
         
-    }
+//     }
 
-    else  if (computerChoice == 'rock' && humanChoice == 'scissor') {
-        computerScore++
-        alert('the computer won rock beats scissor')
-        console.log(computerChoice);
+//     else  if (computerChoice == 'rock' && humanChoice == 'scissor') {
+//         computerScore++
+//         alert('the computer won rock beats scissor')
+//         console.log(computerChoice);
 
-}
+// }
 
-else if(computerChoice == 'scissor' && humanChoice == 'rock') {
-    humanScore++
-    alert('you won  rock beats scissor')
-    console.log(computerChoice);
+// else if(computerChoice == 'scissor' && humanChoice == 'rock') {
+//     humanScore++
+//     alert('you won  rock beats scissor')
+//     console.log(computerChoice);
     
-}
+// }
 
-else if(computerChoice == 'paper' && humanChoice == 'scissor') {
-    humanScore++
-    alert('you won  scissor beats paper')
-    console.log(computerChoice);
+// else if(computerChoice == 'paper' && humanChoice == 'scissor') {
+//     humanScore++
+//     alert('you won  scissor beats paper')
+//     console.log(computerChoice);
     
-}
+// }
 
-else if(computerChoice == 'scissor' && humanChoice == 'paper') {
-    computerScore++
-    alert('the computer won  scissor beats paper')
-    console.log(computerChoice);
+// else if(computerChoice == 'scissor' && humanChoice == 'paper') {
+//     computerScore++
+//     alert('the computer won  scissor beats paper')
+//     console.log(computerChoice);
     
-}
+// }
 
-}
+// }
 
 
-const playGame = () =>{
-for (let i = 0; i < 5; i++) {
-    playerRound()
+// // const playGame = () =>{
+// // for (let i = 0; i < 5; i++) {
+// //     playerRound()
     
-}
+// // }
 
-if (humanScore > computerScore) {
-    alert('yey you won the game')
-}
-else if (humanScore === computerScore) {
-    alert('the 5 rounds  endded in tie, how come ? ')
+// // if (humanScore > computerScore) {
+// //     alert('yey you won the game')
+// // }
+// // else if (humanScore === computerScore) {
+// //     alert('the 5 rounds  endded in tie, how come ? ')
     
-}
+// // }
 
-else{
-    alert('sorry the computer won, good luck next time')
-}
-
-
+// // else{
+// //     alert('sorry the computer won, good luck next time')
+// // }
 
 
-}
 
-playGame();
+
+// // }
+
+// // playGame();
 
